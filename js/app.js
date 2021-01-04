07c745ee8971a98e0c7d0185dfe5f40b34a3feab
eiadnimer <eiadnimer_swaidat@yahoo.com>
2021-01-04 16:21:38 +0200

    adding css & js files to my page

---
+++ js/app.js
@@ -0,0 +1,68 @@
+'use strict'
+var userName = prompt('how is speaking !?');
+alert(' welcome ' + userName + ' to my little page ')
+
+var myname = prompt('my name is batool .. is that correct ?');
+if (myname.toLowerCase() === 'y' || myname.toLowerCase() === 'yes') {
+    //console.log('its WRONG answer , come on batool !!');
+    alert('im very sad walla :( ')
+}
+else if (myname.toLowerCase() === 'n' || myname.toLowerCase() === 'no') {
+    //console.log('correct answer');
+    alert(' correct answer , thank you walla')
+
+}
+else {
+    alert('invalid input');
+}
+
+var myfavclub = prompt(' i am a MADRIDISTA .. so i am a barcelona fan ?');
+if (myfavclub.toLowerCase() === 'y' || myfavclub.toLowerCase() === 'yes') {
+   // console.log('you have to watch bein sport batool ');
+    alert(' of course not ya batool !')
+}
+else if (myfavclub.toLowerCase() === 'n' || myfavclub.toLowerCase() === 'no') {
+    //console.log('correct answer .. finally :D');
+    alert(' correct answer , you should become a madridista :D')
+}
+else {
+    alert('invalid input');
+}
+
+var myfavmeal = prompt(' do you love MANSAF ?');
+if (myfavmeal.toLowerCase() === 'y' || myfavmeal.toLowerCase() === 'yes') {
+    //console.log('correct answer ');
+    alert(' correct answer , me too :D ')
+}
+else if (myfavmeal.toLowerCase() === 'n' || myfavmeal.toLowerCase() === 'no') {
+    //console.log('kef 3aysheh ente ? ');
+    alert('y3ne i cant understand WHY !')
+}
+else {
+    alert('invalid input');
+}
+var myfavplayer = prompt(' is my favorite player is zidane ?');
+if (myfavplayer.toLowerCase() === 'y' || myfavplayer.toLowerCase() === 'yes') {
+   // console.log('correct answer ');
+    alert(' correct answer , he is the best of all time ')
+}
+else if (myfavplayer.toLowerCase() === 'n' || myfavplayer.toLowerCase() === 'no') {
+    //console.log('laaaa3333 ');
+    alert('-_-')
+}
+else {
+    alert('invalid input');
+}
+var thisisyours = prompt(' is your favorite color is blue ? ');
+if (thisisyours.toLowerCase() === 'y' || thisisyours.toLowerCase() === 'yes') {
+    //console.log('correct answer ');
+    alert(' correct answer , also me :D ')
+}
+else if (thisisyours.toLowerCase() === 'n' || thisisyours.toLowerCase() === 'no') {
+    //console.log('its ok ');
+    alert('make it blue ')
+}
+else {
+    alert('invalid input');
+}
+alert(' thank you for your time ' + userName )
