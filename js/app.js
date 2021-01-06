cb97b356ed59eb6c51cec6f2e85f233430ec4b83
eiadnimer <eiadnimer_swaidat@yahoo.com>
2021-01-06 15:42:54 +0200

    updating lab

--- js/app.js
+++ js/app.js
@@ -1,6 +1,74 @@
 'use strict'
+var userName = prompt('how is speaking !?');
+alert(' welcome ' + userName + ' to my little page ')
+
+var myname = prompt('my name is batool .. is that correct ?');
+if (myname.toLowerCase() === 'y' || myname.toLowerCase() === 'yes') {
+    //console.log('its WRONG answer , come on batool !!');
+    alert('im very sad walla :( ')
+}
+else if (myname.toLowerCase() === 'n' || myname.toLowerCase() === 'no') {
+    //console.log('correct answer');
+    alert(' correct answer , thank you walla')
+
+}
+else {
+    alert('invalid input');
+}
+
+var myfavclub = prompt(' i am a MADRIDISTA .. so i am a barcelona fan ?');
+if (myfavclub.toLowerCase() === 'y' || myfavclub.toLowerCase() === 'yes') {
+   // console.log('you have to watch bein sport batool ');
+    alert(' of course not ya batool !')
+}
+else if (myfavclub.toLowerCase() === 'n' || myfavclub.toLowerCase() === 'no') {
+    //console.log('correct answer .. finally :D');
+    alert(' correct answer , you should become a madridista :D')
+}
+else {
+    alert('invalid input');
+}
+
+var myfavmeal = prompt(' do you love MANSAF ?');
+if (myfavmeal.toLowerCase() === 'y' || myfavmeal.toLowerCase() === 'yes') {
+    //console.log('correct answer ');
+    alert(' correct answer , me too :D ')
+}
+else if (myfavmeal.toLowerCase() === 'n' || myfavmeal.toLowerCase() === 'no') {
+    //console.log('kef 3aysheh ente ? ');
+    alert('y3ne i cant understand WHY !')
+}
+else {
+    alert('invalid input');
+}
+var myfavplayer = prompt(' is my favorite player is zidane ?');
+if (myfavplayer.toLowerCase() === 'y' || myfavplayer.toLowerCase() === 'yes') {
+   // console.log('correct answer ');
+    alert(' correct answer , he is the best of all time ')
+}
+else if (myfavplayer.toLowerCase() === 'n' || myfavplayer.toLowerCase() === 'no') {
+    //console.log('laaaa3333 ');
+    alert('-_-')
+}
+else {
+    alert('invalid input');
+}
+var thisisyours = prompt(' is your favorite color is blue ? ');
+if (thisisyours.toLowerCase() === 'y' || thisisyours.toLowerCase() === 'yes') {
+    //console.log('correct answer ');
+    alert(' correct answer , also me :D ')
+}
+else if (thisisyours.toLowerCase() === 'n' || thisisyours.toLowerCase() === 'no') {
+    //console.log('its ok ');
+    alert('make it blue ')
+}
+else {
+    alert('invalid input');
+}
+alert(' thank you for your time ' + userName )
 alert(' you have 4 attempts to get the correct answer.')
-// guess= parseInt(guess);
+var score =0;
+var total;
 var correctanswer = 55;
 var attempts = 4;
 var guesses = 0;
@@ -17,6 +85,8 @@ function guessmyNum(){
             count++;
             correctanswer++;
             alert('AWESOME!! you got me')
+            score++;
+
             break;
         }else if (guess<correctanswer){
             alert('too low');
@@ -25,38 +95,21 @@ function guessmyNum(){
 }
 guessmyNum();
 
-var correct = false;
-var totalguesses = 0;
-var players = ['Zidane','Messi','CR7','Henry','Marcelo'];
-var totalattempts = 6;
-var usenN ;
-function guessmyplayes () {
-  for (var i = 0; i < totalattempts; i++) {
-    usenN = prompt('Can you guess my best players of all time ? ' + i + ' attempts');
-    for (var s = 0; s < players.length; s++) {
-      if (players[s].toLowerCase() === usenN.toLowerCase()) {
-        alert('good job' + players.toString());
-        correct = true;
-        totalguesses++;
-             correctanswer++;
-            break;
-      }
-    }
-    if (correct){
-      correctanswers++;
-      break;
-    }
-    if(totalattempts === i + 1) {
-      alert('your answer is not correct ' + players.toString());
-      correct = true;
-    }
-    else {usenN = prompt('your answer is not correct .. try again please');
-    }
-  }
+var players = ['Zidane','Messi','CR7'];
+for (var i=1; i<=6;i++){
+  var userN = prompt('Can you guess my best players of all time ?');  
+    if(userN == players[0] || userN == players[1] || userN == players[2]){
+      alert('nice answer');
+        score++;
+        break;
+        }
+        else   {
+            alert('wrong'); 
+        }
 }
-guessmyplayes();
-console.log(correctanswers)
-alert('You got ' + correctanswers + ' correct!!');
+
+alert('my best players is '  + players);
+alert('your score is ' + score);
 
 // var userName = prompt('how is speaking !?');
 // alert(' welcome ' + userName + ' to my little page ')
